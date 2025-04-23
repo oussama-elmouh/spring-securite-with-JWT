e6a9ad47139a997ab59cb1b91cc64c39770eab19
EL MOUH Oussama <ouss123maelmouh@gmail.com>
2025-04-23 16:48:08 +0200

    create project

---
+++ src/main/java/org/sid/sec_service/SecServiceApplication.java
@@ -0,0 +1,13 @@
+package org.sid.sec_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sid/sec_service/SecServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.sec_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
