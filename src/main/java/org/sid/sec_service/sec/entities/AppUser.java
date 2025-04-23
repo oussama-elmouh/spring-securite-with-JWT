eeae674a16105c95ea9cac81b567c59871a2434f
EL MOUH Oussama <ouss123maelmouh@gmail.com>
2025-04-23 17:19:04 +0200

    create AppRole et AppUSer

--- src/main/java/org/sid/sec_service/sec/entities/AppUser.java
+++ src/main/java/org/sid/sec_service/sec/entities/AppUser.java
@@ -1,4 +1,21 @@
 package org.sid.sec_service.sec.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Collection;
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AppUser {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    //un utilisateur peut avoir plusieur role et un role concerne plusieur utilisateur
+    @ManyToMany
+    private Collection<AppRole>appRoles;
 }
