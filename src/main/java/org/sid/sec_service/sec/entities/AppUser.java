2ccb6b8eca45f53436e8cf7f669bc26a011a0ecf
EL MOUH Oussama <ouss123maelmouh@gmail.com>
2025-04-27 19:46:17 +0200

    ajouter des methodes de sauvgarde

--- src/main/java/org/sid/sec_service/sec/entities/AppUser.java
+++ src/main/java/org/sid/sec_service/sec/entities/AppUser.java
@@ -1,6 +1,8 @@
 package org.sid.sec_service.sec.entities;
 
 import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,6 +17,8 @@ public class AppUser {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
+    //masquer le password
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
     //un utilisateur peut avoir plusieur role et un role concerne plusieur utilisateur
     @ManyToMany(fetch = FetchType.EAGER)
@@ -22,11 +26,6 @@ public class AppUser {
     public Collection<AppRole> getAppRoles() {
         return appRoles;
     }
-  /*  public AppUser(Long id, String username, String password, Collection<AppRole> appRoles) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-        this.appRoles = appRoles;
-    }*/
+
 
 }
--- src/main/java/org/sid/sec_service/sec/web/AccountRestController.java
+++ src/main/java/org/sid/sec_service/sec/web/AccountRestController.java
@@ -1,8 +1,12 @@
 package org.sid.sec_service.sec.web;
 
+import lombok.Data;
+import org.sid.sec_service.sec.entities.AppRole;
 import org.sid.sec_service.sec.entities.AppUser;
 import org.sid.sec_service.sec.service.AccountService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,6 +24,24 @@ public class AccountRestController {
         return accountService.listUsers();
     }
 
+    @PostMapping(path="/users")
+    public AppUser SaveUser(@RequestBody AppUser appUser){
+        return accountService.addNewUser(appUser);
+    }
 
+    @PostMapping(path="/roles")
+    public AppRole SaveRole(@RequestBody AppRole appRole){
+        return accountService.addNewRole(appRole);
+    }
 
+    @PostMapping(path="/addRoleToUser")
+    public void addRoleToUser(@RequestBody RoleUserForm roleUserForm){
+        accountService.addRoleToUser(roleUserForm.getUsername(),roleUserForm.getRoleName());
+    }
+
+}
+@Data
+class RoleUserForm{
+    private String username;
+    private String roleName;
 }
